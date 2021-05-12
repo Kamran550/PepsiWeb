9d3b8ae333d3fb90b2364d6553eb92cf90f72401
Kamran550 <Kamranferzeliyev01@gmail.com>
2021-05-12 16:29:35 +0400

    pepsi-webbb

---
+++ index.js
@@ -0,0 +1,8 @@
+function changePepsi (anything){
+    document.querySelector(".pepsi").src = anything
+}
+
+function changeColor(color){
+    const reng  = document.querySelector('body')
+    reng.style.background = color
+}
